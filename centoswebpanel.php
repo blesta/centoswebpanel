ff19fc6a2d05e18dc15ed467c3b6ea29640cfe08
Temporary <Temporary@Paul-XPS>
2019-12-03 13:20:09 -0800

    Updated to use library for hostname validation

--- centoswebpanel.php
+++ centoswebpanel.php
@@ -1,4 +1,5 @@
 <?php
+use Blesta\Core\Util\Validate\Server;
 /**
  * CentOS WebPanel Module.
  *
@@ -13,7 +14,7 @@ class Centoswebpanel extends Module
     /**
      * @var string The version of this module
      */
-    private static $version = '1.2.0';
+    private static $version = '1.3.0';
     /**
      * @var string The authors of this module
      */
@@ -1213,14 +1214,8 @@ class Centoswebpanel extends Module
      */
     public function validateHostName($host_name)
     {
-        if (strlen($host_name) > 255) {
-            return false;
-        }
-
-        return $this->Input->matches(
-            $host_name,
-            '/^([a-z0-9]|[a-z0-9][a-z0-9\-]{0,61}[a-z0-9])(\.([a-z0-9]|[a-z0-9][a-z0-9\-]{0,61}[a-z0-9]))+$/i'
-        );
+        $validator = new Server();
+        return $validator->isDomain($host_name);
     }
 
     /**
